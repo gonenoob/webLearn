29b56123776fc1b756266bd3c66a5db8a537a330
maojunda <wb-maojunda@taobao.com>
2016-02-15 16:35:49 +0800

    ref

--- reflow.js
+++ reflow.js
@@ -1,3 +1,5 @@
+https://segmentfault.com/a/1190000002629708
+
 对于DOM结构中的各个元素都有自己的盒子（模型），
 这些都需要浏览器根据各种样式（浏览器的、开发人员定义的等）来计算并根据计算结果将元素放到它该出现的位置，
 这个过程称之为reflow；
@@ -18,3 +20,25 @@ DOM Tree 里的每个结点都会有 reflow 方法，一个结点的 reflow 很�
 当你 Resize 窗口的时候（移动端没有这个问题），或是滚动的时候。
 当你修改网页的默认字体时。
 注：display:none 会触发 reflow，而 visibility:hidden 只会触发 repaint，因为没有发现位置变化。
+
+
+优化
+
+Reflow是不可避免的，只能将Reflow对性能的影响减到最小,给出下面几条建议：
+
+1.不要一条一条地修改 DOM 的样式。与其这样，还不如预先定义好 css 的 class，然后修改 DOM 的 className：
+	// 不好的写法
+	var left = 10,
+	top = 10;
+	el.style.left = left + "px";
+	el.style.top  = top  + "px";
+	// 推荐写法
+	el.className += " theclassname";
+2.把 DOM 离线后修改。如：
+a> 使用 documentFragment 对象在内存里操作 DOM。
+b> 先把 DOM 给 display:none (有一次 repaint)，然后你想怎么改就怎么改。比如修改 100 次，然后再把他显示出来。
+c>  clone 一个 DOM 节点到内存里，然后想怎么改就怎么改，改完后，和在线的那个的交换一下。
+3.不要把 DOM 节点的属性值放在一个循环里当成循环里的变量。不然这会导致大量地读写这个结点的属性。
+4.尽可能的修改层级比较低的 DOM节点。当然，改变层级比较底的 DOM节点有可能会造成大面积的 reflow，但是也可能影响范围很小。
+5.为动画的 HTML 元件使用 fixed 或 absoult 的 position，那么修改他们的 CSS 是会大大减小 reflow 。
+6.千万不要使用 table 布局。因为可能很小的一个小改动会造成整个 table 的重新布局。
